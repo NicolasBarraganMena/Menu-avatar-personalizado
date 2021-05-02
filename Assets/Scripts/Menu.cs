53bc18d5f516744b5c878c0891beb14b9cfcd6bc
Nicolas Barragan <nicolas.barragan@upb.edu.co>
2021-05-01 19:00:29 -0500

    Rotar y Cambiar sexo personajes
    Se creo el script del Menú, y se controla el giro del avatar y el cambio de sexo.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [Header("Avatar Settings")]
+    //Referencia a los objetos
+    public GameObject[] avatars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Rotar avatar
+    public void Rotar(int direccion)
+    {
+        for (int i = 0; i < avatars.Length; i++)
+        {
+            avatars[i].transform.Rotate(0, direccion * 30, 0, Space.World);
+        }
+    }
+
+    //Cambiar de sexo
+    public void CambiarSexo(int sexoActual)
+    {
+        for (int i = 0; i < avatars.Length; i++)
+        {
+            if (sexoActual != i)
+            {
+                avatars[i].SetActive(false);
+            }
+            else
+            {
+                avatars[i].SetActive(true);
+            }
+        }
+    }
+}
