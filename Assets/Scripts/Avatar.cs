39d5a7614515eec2817d951b160ce4972ba7eb6f
Nicolas Barragan <nicolas.barragan@upb.edu.co>
2021-05-01 19:23:43 -0500

    Cambiar cabeza avatar

---
+++ Assets/Scripts/Avatar.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar : MonoBehaviour
+{
+
+    [Header("Head Settings")]
+    //Control cambio de cabeza
+    public GameObject[] Headtypes;
+    public int headValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Valores iniciales del avatar
+        headValue = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //cambiar las cabezas  de los avatares
+    public void CambiarCabeza()
+    {
+        if (headValue < Headtypes.Length - 1)
+        {
+            headValue++;
+        }
+        else
+        {
+            headValue = 0;
+        }
+
+        for (int j = 0; j < Headtypes.Length; j++)
+        {
+            if (headValue != j)
+            {
+                Headtypes[j].SetActive(false);
+            }
+            else
+            {
+                Headtypes[j].SetActive(true);
+            }
+        }
+    }
+}
