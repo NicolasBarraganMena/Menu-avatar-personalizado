9b16989142c4c5e520e50635b0f82a3316561542
Nicolas Barragan <nicolas.barragan@upb.edu.co>
2021-05-01 19:36:58 -0500

    Cambiar color piel

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -10,19 +10,70 @@ public class Avatar : MonoBehaviour
     public GameObject[] Headtypes;
     public int headValue;
 
+    [Header("Skin Settings")]
+    //Control cambio de piel
+    public Material[] skins;
+    public GameObject[] skinParts;
+    public int[] skinI;
+    public int skinValue;
+    Renderer renderer;
+    private Material[] materials;
 
     // Start is called before the first frame update
     void Start()
     {
         //Valores iniciales del avatar
         headValue = 0;
+        skinValue = 0;
 
+        //Funciones manejos materiales
+        ControlPiel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Funciones manejos materiales
+        ControlPiel();
+
+    }
+
+    //cambiar el color de piel
+    public void CambiarPiel()
+    {
+        if (skinValue < skins.Length - 1)
+        {
+            skinValue = skinValue + 1;
+        }
+        else
+        {
+            skinValue = 0;
+        }
+    }
+
+    //control material piel
+    void ControlPiel()
+    {
+        for (int i = 0; i < skinParts.Length; i++)
+        {
+            if (i == 0)
+            {
+                renderer = Headtypes[headValue].GetComponent<Renderer>();
+                materials = renderer.sharedMaterials;
+                materials[0] = skins[skinValue];
+                renderer.enabled = true;
+                renderer.sharedMaterials = materials;
+            }
+            else
+            {
+                renderer = skinParts[i].GetComponent<Renderer>();
+                materials = renderer.sharedMaterials;
+                materials[skinI[i]] = skins[skinValue];
+                renderer.enabled = true;
+                renderer.sharedMaterials = materials;
+            }
+        }
     }
 
     //cambiar las cabezas  de los avatares
